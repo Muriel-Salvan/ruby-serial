f1308507ea9af1d74e35a1767ec2ae27de2f79a1
Muriel Salvan <Muriel@X-Aeon.com>
2013-07-05 17:32:30 +0200

    Added some test cases on user objects.

--- test/ruby-serial/testcases/user_objects.rb
+++ test/ruby-serial/testcases/user_objects.rb
@@ -44,6 +44,22 @@ module RubySerialTest
         assert_equal 3, obj2.attr3
       end
 
+      # Don't serialize 2 attributes
+      class Serialized_Except_2 < Serialized_All
+        dont_rubyserial :attr2
+        dont_rubyserial :attr1
+      end
+      def_test 'except_2' do
+        obj1 = Serialized_Except_2.new
+        obj1.attr1 = 1
+        obj1.attr2 = 2
+        obj1.attr3 = 3
+        obj2 = ruby_serial(obj1)
+        assert_equal nil, obj2.attr1
+        assert_equal nil, obj2.attr2
+        assert_equal 3, obj2.attr3
+      end
+
       # Don't serialize a list of attributes
       class Serialized_Except_List < Serialized_All
         dont_rubyserial :attr1, :attr2
@@ -70,6 +86,22 @@ module RubySerialTest
         obj1.attr3 = 3
         obj2 = ruby_serial(obj1)
         assert_equal nil, obj2.attr1
+        assert_equal 2, obj2.attr2
+        assert_equal nil, obj2.attr3
+      end
+
+      # Serialize only 2 attributes
+      class Serialized_Only_2 < Serialized_All
+        rubyserial_only :attr2
+        rubyserial_only :attr1
+      end
+      def_test 'only_2' do
+        obj1 = Serialized_Only_2.new
+        obj1.attr1 = 1
+        obj1.attr2 = 2
+        obj1.attr3 = 3
+        obj2 = ruby_serial(obj1)
+        assert_equal 1, obj2.attr1
         assert_equal 2, obj2.attr2
         assert_equal nil, obj2.attr3
       end
