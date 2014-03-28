a30c982964d6e0598a19cf8508b1fe3303424811
Muriel Salvan <Muriel@X-Aeon.com>
2014-03-28 14:33:04 +0100

    Brought back 1.9 compatibility

--- lib/ruby-serial/versions/1/deserializer.rb
+++ lib/ruby-serial/versions/1/deserializer.rb
@@ -67,7 +67,9 @@ module RubySerial
                 else
                   # We deserialize a home-made object
                   # Instantiate the needed class
-                  new_obj = ((container_to_fill.nil?) ? Module.const_get(decoded_obj[OBJECT_CLASSNAME_REFERENCE]).allocate : container_to_fill)
+                  # TODO: Replace with the commented line as soon as 1.9 is not supported anymore
+                  # new_obj = ((container_to_fill.nil?) ? Module.const_get(decoded_obj[OBJECT_CLASSNAME_REFERENCE]).allocate : container_to_fill)
+                  new_obj = ((container_to_fill.nil?) ? eval(decoded_obj[OBJECT_CLASSNAME_REFERENCE]).allocate : container_to_fill)
                   instance_vars = {}
                   decoded_obj[OBJECT_CONTENT_REFERENCE].each do |var_name, serialized_value|
                     instance_vars[var_name] = get_original_rec(serialized_value)
@@ -82,7 +84,9 @@ module RubySerial
                 obj_id = decoded_obj[OBJECT_ID_REFERENCE]
                 if @decoded_shared_objs[obj_id].nil?
                   # Instantiate it already for cyclic decoding (avoids infinite loops)
-                  @decoded_shared_objs[obj_id] = Module.const_get(@serialized_shared_objs[obj_id][0]).allocate
+                  # TODO: Replace with the commented line as soon as 1.9 is not supported anymore
+                  # @decoded_shared_objs[obj_id] = Module.const_get(@serialized_shared_objs[obj_id][0]).allocate
+                  @decoded_shared_objs[obj_id] = eval(@serialized_shared_objs[obj_id][0]).allocate
                   get_original_rec(@serialized_shared_objs[obj_id][1], @decoded_shared_objs[obj_id])
                 end
                 return @decoded_shared_objs[obj_id]
